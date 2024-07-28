e3efa1ec2ceefa954528ce10720e350abd0d9cbb
Azalya Rahmatika <azalyarahmatika@gmail.com>
2024-07-28 15:37:57 +0300

    fixing footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer({theme}) {
   return (
-    <div className="text-desaturated_blue_text font-inter text-sm pt-3 font-semibold">
+    <div className="text-desaturated_blue_text font-inter text-sm pt-3 font-semibold text-center w-full">
       Challenge by <a className={`${theme ? "text-white" : "text-very_dark_blue_text"}`}  href="https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H/hub"target="_blank">Frontend Mentor</a>. 
       Coded by <a className={`${theme ? "text-white" : "text-very_dark_blue_text"}`} href="https://azalyarahmatika.com" target="_blank">Azalya Rahmatika</a>.
     </div>
